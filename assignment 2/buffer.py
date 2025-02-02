325fd60fa412fd539f598977e2cea8a3ea6ed3fe
Cora Mikolajczyk <coramikol1@lsu.edu>
2025-02-02 13:36:46 -0600

    update

---
+++ assignment 2/buffer.py
@@ -0,0 +1,29 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": None,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "language_info": {
+   "name": "python"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
+import arcpy
+arcpy.analysis.Buffer(
+    in_features="no_retail.shp",
+    out_feature_class="./retail_buffer.shp",
+    buffer_distance_or_field="500 Meters",
+    line_side="FULL",
+    line_end_type="ROUND",
+    dissolve_option="NONE",
+    dissolve_field=None,
+    method="PLANAR"
+)
